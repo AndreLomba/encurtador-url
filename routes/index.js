25ef6a78d1ecbaf1978cde5982351597aa5c8063
AndreLomba <landrelomba@gmail.com>
2022-08-24 10:28:25 -0300

    Alterando o projeto para os métodos retornarem um JSON, eu tinha criado o projeto com views para o usuário navegar.

--- routes/index.js
+++ routes/index.js
@@ -8,29 +8,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Encurtador' });
 });
 
-// Redireciona para URL original a URL encurtada recebida
-router.get('/:url_encurtada', async (req, res, next) => {
-  const url_encurtada = req.params.url_encurtada;
- 
-  const resultado = await Link.findOne({ where: { url_encurtada } });
-  if (!resultado) return res.sendStatus(404);
- 
-  resultado.hits++;
-  await resultado.save();
- 
-  res.redirect(resultado.url_original);
-})
-
-// Busca url_encurtada no banco pelo id
-router.post('/buscar', async (req, res, next) => {
-  id = req.body.id;
-  
-  resultado = [];
+/* Busca url_encurtada no banco pelo id */
+router.get('/buscar/:id', async (req, res, next) => {
+  id = req.params.id;
   resultado = await Link.findAll({ where: { id } });
-  res.render('details', resultado[0] ? resultado[0].dataValues : []);
+  res.json(resultado[0] ? resultado[0].dataValues : [{'Mensagem': 'Nenhum registro encontrado.'}]);
 });
 
-// Busca url_encurtadas no banco pela data
+/* Recebe como parametro post $data (dd/mm/yyyy) para listar as urls */
 router.post('/buscardata', async (req, res, next) => {
   const Op = Sequelize.Op; 
   data_original = req.body.data;
@@ -40,19 +25,9 @@ router.post('/buscardata', async (req, res, next) => {
   const startedDate = new Date(data + " 00:00:00");
   const endDate = new Date(data + " 20:59:59");
 
-  resultado = [];
   resultado = await Link.findAll({where : {"createdAt" : {[Op.between] : [startedDate , endDate ]}}});
-  res.render('list', resultado.dataValues);
+  res.json(resultado[0] ? resultado : [{'Mensagem': 'Nenhum registro encontrado.'}]);
 });
-
-/* Gera nova URL */
-function generationUrl() {
-  let text = '';
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i < 5; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  return text;
-}
 
 /* Recebe como parametro post $url URL para encurtar */
 router.post('/new', async (req, res, next) => {
@@ -64,7 +39,16 @@ router.post('/new', async (req, res, next) => {
     url_original
   });
 
-  res.render('stats', resultado.dataValues);
+  res.json(resultado ? resultado.dataValues : [{'Mensagem': 'Não foi possível gerar a URL.'}]);
 });
+
+/* Gera nova URL */
+function generationUrl() {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
 
 module.exports = router;
